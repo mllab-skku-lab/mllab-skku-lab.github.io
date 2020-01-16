cc4d8675b819ce1466018dd20bd63e30de5e38cf
mllab-skku-lab <59719160+mllab-skku-lab@users.noreply.github.com>
2020-01-16 21:57:17 +0900

    Update mllab.js

--- mllab.js
+++ mllab.js
@@ -45,3 +45,47 @@
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
+
+    var slideIndex = 0;
+      function currentSlide(n) {
+        showSlides(slideIndex = n);
+      }
+
+      function showSlides(n) {
+        var i;
+        var slides = document.getElementsByClassName("mySlides");
+        var dots = document.getElementsByClassName("dot");
+        /*if (n > slides.length) {slideIndex = 1}
+        if (n < 1) {slideIndex = slides.length}
+        */
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex-1].style.display = "block";
+        dots[slideIndex-1].className += " active";
+      }
+
+
+
+      window.onload = function() {
+        setInterval(function() {
+          if (slideIndex == 3) {
+            slideIndex = 0;
+          }
+          var i;
+          var slides = document.getElementsByClassName("mySlides");
+          var dots = document.getElementsByClassName("dot");
+          for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+          }
+          slideIndex++;
+          for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+          }
+          slides[slideIndex-1].style.display = "block";
+          dots[slideIndex-1].className += " active";
+        }, 2000);
+      }
